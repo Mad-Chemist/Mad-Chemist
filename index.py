b9acc7691d8e3c818a416a53826db6993aa90ab7
Vico Bertogli III <vico@b3wd.com>
2025-04-29 22:42:23 -0400

    So close to perfection

--- index.py
+++ index.py
@@ -409,8 +409,8 @@ def justify_format(root, element_id, new_text, length=0):
     new_text = str(new_text)
     find_and_replace(root, element_id, new_text)
     just_len = max(0, length - len(new_text))
-    if just_len <= 2:
-        dot_string = ' ' + ('.' * just_len)
+    if just_len = 0:
+        dot_string = ''
     else:
         dot_string = ' ' + ('.' * just_len) + ' '
     find_and_replace(root, f"{element_id}_dots", dot_string)

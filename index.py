2654c1fdc6e42f778613a426ecaa5170cb3d2202
Vico Bertogli III <vico@b3wd.com>
2025-04-29 22:53:03 -0400

    last formatting tweak?

--- index.py
+++ index.py
@@ -411,6 +411,8 @@ def justify_format(root, element_id, new_text, length=0):
     just_len = max(0, length - len(new_text))
     if just_len == 0:
         dot_string = ' '
+    elif just_len == 1:
+        dot_string = '. '
     else:
         dot_string = ' ' + ('.' * just_len) + ' '
     find_and_replace(root, f"{element_id}_dots", dot_string)

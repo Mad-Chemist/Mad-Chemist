1c43ef184b86fc4d7c226584a4a4e81830109d1e
Vico Bertogli III <vico@b3wd.com>
2025-04-30 15:54:37 -0400

    Better vertical positioning

--- index.py
+++ index.py
@@ -369,13 +369,14 @@ def draw_avatar_color_ascii(root, ascii):
     start_y = 30
     line_height = 20
     un_pad = int((ASCII_GEN_COLS-ASCII_PRINT_COLS)/2)
+    total_line_offset = 0 if total_lines <= ASCII_MAX_LINES else int((total_lines-ASCII_MAX_LINES) /2)
     avatar = root.find(f".//*[@id='avatar']")
     # Clear any existing content
     for child in avatar:
         avatar.remove(child)
 
     avatar_rows = extract_html_for_ascii(ascii)
-    for row_idx, row in enumerate(avatar_rows):
+    for row_idx, row in enumerate(avatar_rows[total_line_offset:-total_line_offset]):
         x_pos = start_x
         text_elem = etree.SubElement(avatar, "tspan", x=str(x_pos), y=str(start_y+(row_idx*line_height)))
         for text, color in row[un_pad:-un_pad]:
